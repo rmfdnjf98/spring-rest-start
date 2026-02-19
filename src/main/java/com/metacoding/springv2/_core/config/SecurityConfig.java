a6bc2adfb661d2e1854011098b1e9ee919789066
KJcording <rmfdnjf98@gmil.com>
2026-02-19 10:09:40 +0900

    UsernamePasswordAuthenticationFilter  까지 배움

--- src/main/java/com/metacoding/springv2/_core/config/SecurityConfig.java
+++ src/main/java/com/metacoding/springv2/_core/config/SecurityConfig.java
@@ -4,13 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-//@Configuration
+import com.metacoding.springv2._core.filter.JwtAuthorizationFilter;
+
+@Configuration
 public class SecurityConfig {
 
     // 시큐리티 필터 등록
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 인증/권한 주소 커스터마이징
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll());
+
+        // 폼 로그인 비활성화 ( POST : x-www-form-urlencoded : username, password)
+        http.formLogin(f -> f.disable());
+
+        // Security가 베이직 인증 활성화(request 할 때마다 username, password를 요구한다.) 시킴 이걸 꺼줘야 한다.
+        http.httpBasic(b -> b.disable());
+
+        // 인증 필터를 변경
+        http.addFilterBefore(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
